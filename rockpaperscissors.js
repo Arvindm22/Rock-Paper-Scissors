56785a1dfde71ffe80baaa5dcbf27e2c11a7ebd5
Arvind Muralidharan <arvimurali.99@gmail.com>
2022-10-21 13:39:42 +0530

    Updated the code

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -8,21 +8,34 @@ function getComputerChoice(){
 //Function to play single round of Rock Paper Scissors
 
 function playRound(playerSelection, computerSelection ,playerName_1){
+    winner = undefined;
     if((playerSelection === 'rock' && computerSelection === 'scissor') || (playerSelection === 'paper' && computerSelection === 'rock') ||(playerSelection === 'scissor' && computerSelection === 'paper'))
     {
+        winner = playerName_1
         return `${playerName_1} Congrats!! You have won the game!!!!!!!!!!!!!! `;
     }
     else if ((computerSelection === 'rock' && playerSelection === 'scissor') || (computerSelection === 'paper' && playerSelection === 'rock') ||(computerSelection === 'scissor' && playerSelection === 'paper'))
     {
+        winner = 'computer'
         return `${playerName_1} Sorry!!! You have lost the game!!!!!!!!!!!!!! `;}
     else
-    {
+    {   
+        winner = 'draw'
         return  `${playerName_1} Its a draw !!!!! `;
     }
 }
 
+// Function to play the game for a given no of rounds
+
 playerName_1 = prompt("Enter your name:");
-playerSelection = prompt("What is your choice:Rock/Paper/Scissor");
+
+playerSelection = prompt("What is your choice:Rock/Paper/Scissor").toLowerCase();
+if( playerSelection!=="rock" ||  playerSelection!=="paper" || playerSelection!=="scissor")
+{
+    alert("Please enter a valid choice!!");
+    playerSelection = prompt("What is your choice:Rock/Paper/Scissor").toLowerCase();
+}
+
 computerSelection=getComputerChoice();
 winner = playRound(playerSelection, computerSelection,playerName_1);
 console.log(winner);
