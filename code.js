ffc2c9626cc1d7a0fdb7237db3647eb0c0cec89a
Arvind Muralidharan <arvimurali.99@gmail.com>
2022-10-27 16:23:48 +0530

    Added functions for playing the game

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 let playerSelection;
+let computerSelection;
 // Function to randomly choose rock, paper or scissor (Computer's Play) 
 
 function getComputerChoice() {
@@ -6,38 +7,43 @@ function getComputerChoice() {
         return choice[Math.floor(Math.random() * 3)];
 }
 
-let computerSelection ;
 
 //Function to play single round of Rock Paper Scissors
 
 function playRound(playerSelection, computerSelection) {
         let gameWinner = undefined;
+
         if ((playerSelection === 'rock' && computerSelection === 'scissor') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissor' && computerSelection === 'paper')) {
-                gameWinner = info.elements.playerName.value;
+                gameWinner = playerName;
+                score_1 += 1;
                 return gameWinner;
         }
         else if ((computerSelection === 'rock' && playerSelection === 'scissor') || (computerSelection === 'paper' && playerSelection === 'rock') || (computerSelection === 'scissor' && playerSelection === 'paper')) {
                 gameWinner = 'computer'
+                score_2 += 1;
                 return gameWinner;
         }
         else {
-                gameWinner = ' It is a draw.......'
+                gameWinner = 'Draw'
                 return gameWinner;
         }
+        
 }
+
 let info = document.querySelector('form');
 let player = document.querySelector('#player');
 let computer = document.querySelector('#computer');
 score_1 = 0;
 score_2 = 0;
-
+let playerName;
 info.addEventListener('submit', function (e) {
         e.preventDefault();
-        if (!info.elements.playerName.value) {
+        playerName = info.elements.playerName.value;
+        if (!playerName) {
                 alert("Please Enter a Name!!!")
         }
         else {
-                player.innerText = `${info.elements.playerName.value}:${score_1}`
+                player.innerText = `${playerName}:${score_1}`
                 info.remove();
         }
 })
@@ -45,42 +51,86 @@ info.addEventListener('submit', function (e) {
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissor = document.querySelector('#scissor');
+const display = document.querySelector('#result div');
+const b = document.querySelector('#buttons');
 let result;
+let winningScore = 5;
 
 rock.addEventListener('click', () => {
-        if (!info.elements.playerName.value) {
+        if (!playerName) {
                 alert("Please Enter a Name!!!")
         }
-        else{
-        console.log('working');
-        playerSelection = 'rock';
-        computerSelection = getComputerChoice();
-        console.log(computerSelection);
-        result = playRound(playerSelection,computerSelection);
-        console.log(result);
-}})
+        else {
+                console.log('working');
+                playerSelection = 'rock';
+                computerSelection = getComputerChoice();
+                console.log(computerSelection);
+                result = playRound(playerSelection, computerSelection);
+                console.log(result);
+                if (result === playerName) {
+                        player.innerText = `${playerName}:${score_1}`
+                        display.innerText = `${playerName} wins`;
+                }
+                else if (result === 'computer') {
+                        computer.innerText = `Computer:${score_2}`;
+                        display.innerText = `computer wins`;
+                }
+                else {
+                        display.innerText = result;
+                }
+        }
+})
+
 paper.addEventListener('click', () => {
-        if (!info.elements.playerName.value) {
+        if (!playerName) {
                 alert("Please Enter a Name!!!")
         }
-        else{
-        console.log('working');
-        playerSelection = 'paper';
-        computerSelection = getComputerChoice();
-        console.log(computerSelection);
-        result = playRound(playerSelection,computerSelection);
-        console.log(result);
-       
-}})
+        else {
+                console.log('working');
+                playerSelection = 'paper';
+                computerSelection = getComputerChoice();
+                console.log(computerSelection);
+                result = playRound(playerSelection, computerSelection);
+                console.log(result);
+                if (result === playerName) {
+
+                        player.innerText = `${playerName}:${score_1}`;
+                        display.innerText = `${playerName} wins`;
+                }
+                else if (result === 'computer') {
+
+                        computer.innerText = `Computer:${score_2}`;
+                        display.innerText = `computer wins`;
+                }
+                else {
+                        display.innerText = result;
+                }
+
+        }
+})
+
 scissor.addEventListener('click', () => {
-        if (!info.elements.playerName.value) {
+        if (!playerName) {
                 alert("Please Enter a Name!!!")
         }
-        else{
-        console.log('working');
-        playerSelection = 'scissor';
-         computerSelection = getComputerChoice();
-         console.log(computerSelection);
-        result = playRound(playerSelection,computerSelection);
-        console.log(result)
-}})
+        else {
+                console.log('working');
+                playerSelection = 'scissor';
+                computerSelection = getComputerChoice();
+                console.log(computerSelection);
+                result = playRound(playerSelection, computerSelection);
+                console.log(result);
+                if (result === playerName) {
+
+                        player.innerText = `${playerName}:${score_1}`;
+                        display.innerText = `${playerName} wins`;
+                }
+                else if (result === 'computer') {
+
+                        computer.innerText = `Computer:${score_2}`;
+                }
+                else {
+                        display.innerText = result;
+                }
+        }
+})
